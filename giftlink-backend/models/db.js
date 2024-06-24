678f78b410953e56f14b3432e55284a6f9f22c4d
gidgenf <gidgenf@hotmail.com>
2024-06-23 23:44:15 -0400

    second commit

--- giftlink-backend/models/db.js
+++ giftlink-backend/models/db.js
@@ -9,26 +9,15 @@ let dbInstance = null;
 const dbName = "giftdb";
 
 async function connectToDatabase() {
-    if (dbInstance) {
+    if (dbInstance){
         return dbInstance
     };
 
     const client = new MongoClient(url);
 
-    try {
-        // Task 1: Connect to MongoDB
-        await client.connect()
-
-        // Task 2: Connect to database giftDB and store in variable dbInstance
-        dbInstance = client.db(dbName);
-
-        // Task 3: Return database instance
-        return dbInstance;
-
-    } catch (err) {
-        console.error("Error connecting to database:", err);
-        throw err;
-    }
+    await client.connect();
+    dbInstance = client.db(dbName);
+    return dbInstance;
 }
 
 module.exports = connectToDatabase;
