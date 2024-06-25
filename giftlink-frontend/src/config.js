b5ebd44308d8a1900ecf36e49876ad959c5a4894
gidgenf <140871707+gidgenf@users.noreply.github.com>
2024-06-25 18:02:56 -0300

    Update config.js

--- giftlink-frontend/src/config.js
+++ giftlink-frontend/src/config.js
@@ -2,7 +2,7 @@ import React, { useState,useEffect } from 'react';
 //Step 1 - Task 1
 import {urlConfig} from '../src/config';
 //Step 1 - Task 2
-import { useAppContext } from '../context/AuthContext';
+import { useAppContext } from '../AuthContext';
 //Step 1 - Task 3
 import { useNavigate } from 'react-router-dom';
 import './LoginPage.css';
