5e0c0d3f17e8cd579fcdefe1084cc90f0ca8d21a
gidgenf <140871707+gidgenf@users.noreply.github.com>
2024-06-25 19:00:04 -0300

    Update authRoutes.js

--- giftlink-backend/routes/authRoutes.js
+++ giftlink-backend/routes/authRoutes.js
@@ -61,7 +61,7 @@ router.post('/register', async (req, res) => {
 
     //Login Endpoint
 router.post('/login', async (req, res) => {
-    console.log("\n\n Inside login")
+    console.log("\n\n Inside login");
 
     try {
         // const collection = await connectToDatabase();
@@ -70,7 +70,7 @@ router.post('/login', async (req, res) => {
         const theUser = await collection.findOne({ email: req.body.email });
 
         if (theUser) {
-            let result = await bcryptjs.compare(req.body.password, theUser.password)
+            let result = await bcryptjs.compare(req.body.password, theUser.password);
             if(!result) {
                 logger.error('Passwords do not match');
                 return res.status(404).json({ error: 'Wrong pasword' });
