053fd1cc7a57b5b0970847ebeae6d66d4dbf5631
gidgenf <140871707+gidgenf@users.noreply.github.com>
2024-06-25 19:21:51 -0300

    Update LoginPage.js

--- giftlink-frontend/src/components/LoginPage/LoginPage.js
+++ giftlink-frontend/src/components/LoginPage/LoginPage.js
@@ -1,9 +1,6 @@
 import React, { useState,useEffect } from 'react';
-//Step 1 - Task 1
 import {urlConfig} from '../../config';
-//Step 1 - Task 2
 import { useAppContext } from '../../context/AuthContext';
-//Step 1 - Task 3
 import { useNavigate } from 'react-router-dom';
 
 import './LoginPage.css';
@@ -11,13 +8,10 @@ import './LoginPage.css';
 function LoginPage() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-    //Step 1 - Task 4
     const [incorrect, setIncorrect] = useState('');
-    //Step 1 - Task 5
     const navigate = useNavigate();
     const bearerToken = sessionStorage.getItem('bearer-token');
     const { setIsLoggedIn } = useAppContext();
-    //Step 1 - Task 6
     useEffect(() => {
         if (sessionStorage.getItem('auth-token')) {
           navigate('/app')
@@ -25,35 +19,26 @@ function LoginPage() {
       }, [navigate])
     const handleLogin = async (e) => {
         e.preventDefault();
-        //api call
         const res = await fetch(`${urlConfig.backendUrl}/api/auth/login`, {
-            //Step 1 - Task 7
             method: 'POST',
-            //Step 1 - Task 8
           headers: {
             'content-type': 'application/json',
             'Authorization': bearerToken ? `Bearer ${bearerToken}` : '', // Include Bearer token if available
           },
-        //Step 1 - Task 9
           body: JSON.stringify({
             email: email,
             password: password,
           })
         });
-        //Step 2: Task 1
         const json = await res.json();
         console.log('Json',json);
         if (json.authtoken) {
-            //Step 2: Task 2
           sessionStorage.setItem('auth-token', json.authtoken);
           sessionStorage.setItem('name', json.userName);
           sessionStorage.setItem('email', json.userEmail);
-            //Step 2: Task 3
           setIsLoggedIn(true);
-            //Step 2: Task 4
           navigate('/app');
         } else {
-            //Step 2: Task 5
           document.getElementById("email").value="";
           document.getElementById("password").value="";
           setIncorrect("Wrong password. Try again.");
